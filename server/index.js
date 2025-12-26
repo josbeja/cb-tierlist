3ff7c64da52b5d5307372430ac20e700561b10aa
Jose Bejarano <jose.bejarano@redbus.com>
2025-12-26 13:30:42 -0500

    Try port 465 SSL for SMTP connection

--- server/index.js
+++ server/index.js
@@ -19,14 +19,11 @@ const io = new Server(server, {
 // Email transporter setup
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 587,
-    secure: false, // Use TLS
+    port: 465,
+    secure: true, // Use SSL
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
-    },
-    tls: {
-        rejectUnauthorized: true
     }
 });
 
